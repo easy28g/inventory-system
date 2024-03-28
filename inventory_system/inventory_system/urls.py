58f9f8d3e255673b617119806ce2bc41d7fc95fe
azamat <azamat27071998@gmail.com>
2024-03-28 15:10:15 +0600

    swagger

--- inventory_system/inventory_system/urls.py
+++ inventory_system/inventory_system/urls.py
@@ -4,6 +4,17 @@ from rest_framework import routers
 from company.views import CompanyViewSet
 from product.views import ProductViewSet
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='v1',
+        description="API documentation of App",
+    ),
+    public=True,
+)
 
 router = routers.DefaultRouter()
 router.register(r'company', CompanyViewSet, basename='company')
@@ -13,4 +24,5 @@ router.register(r'product', ProductViewSet, basename='product')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
 ]
